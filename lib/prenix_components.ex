a926c3ff95d837a77b214d5a85e7b3e39e4f7f18
Sean Yeoh <dev@seanysx.com>
2023-11-11 16:46:25 +0800

    First commit

---
+++ lib/prenix_components.ex
@@ -0,0 +1,18 @@
+defmodule PrenixComponents do
+  @moduledoc """
+  Documentation for `PrenixComponents`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrenixComponents.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prenix_components_test.exs
@@ -0,0 +1,8 @@
+defmodule PrenixComponentsTest do
+  use ExUnit.Case
+  doctest PrenixComponents
+
+  test "greets the world" do
+    assert PrenixComponents.hello() == :world
+  end
+end
