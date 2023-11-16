a8e9f939769038562285a7e354ee16001fda53f3
Sean Yeoh <dev@seanysx.com>
2023-11-16 23:27:24 +0800

    Add phoenix to deps

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule PrenixComponents.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:phoenix_live_view, "~> 0.20.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
