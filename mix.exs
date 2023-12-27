07ffc13f8e90211b59d3d35510e8e7ca82ae7c23
Sean Yeoh <dev@seanysx.com>
2023-12-27 20:43:43 +0800

    Add mix.exs metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule PrenixComponents.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
     ]
   end
 
@@ -24,6 +26,21 @@ defmodule PrenixComponents.MixProject do
       {:phoenix_live_view, "~> 0.20.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Prebuilt UI components for Phoenix.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Sean Yeoh"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/prenix-components/prenix_components"},
+      files: ~w(assets config lib vendors LICENSE.md mix.exs README.md)
     ]
   end
 end
